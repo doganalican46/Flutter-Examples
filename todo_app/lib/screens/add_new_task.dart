5af015ed6a8330556a8940a825318eb6d86814a2
Ali Can Dogan <76850300+doganalican46@users.noreply.github.com>
2023-09-15 12:13:35 +0300

    Add new task page updated

--- todo_app/lib/screens/add_new_task.dart
+++ todo_app/lib/screens/add_new_task.dart
@@ -17,61 +17,142 @@ class _AddNewTaskScreenState extends State<AddNewTaskScreen> {
     return SafeArea(
       child: Scaffold(
         backgroundColor: HexColor(backgroundColor),
-        body: Column(
-          children: [
-            Container(
-              width: deviceWidth,
-              height: deviceHeight / 10,
-              decoration: const BoxDecoration(
-                color: Colors.purple,
-                image: DecorationImage(
-                    image: AssetImage("lib/assets/images/header.png"),
-                    fit: BoxFit.cover),
-              ),
-              child: Row(
-                children: [
-                  IconButton(
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                    },
-                    icon: const Icon(
-                      Icons.close,
-                      size: 40,
-                      color: Colors.white,
-                    ),
-                  ),
-                  const Expanded(
-                      child: Text(
-                    "Add new Task",
-                    style: TextStyle(
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                width: deviceWidth,
+                height: deviceHeight / 10,
+                decoration: const BoxDecoration(
+                  color: Colors.purple,
+                  image: DecorationImage(
+                      image: AssetImage("lib/assets/images/header.png"),
+                      fit: BoxFit.cover),
+                ),
+                child: Row(
+                  children: [
+                    IconButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      icon: const Icon(
+                        Icons.close,
+                        size: 40,
                         color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 21),
-                    textAlign: TextAlign.center,
-                  ))
-                ],
+                      ),
+                    ),
+                    const Expanded(
+                        child: Text(
+                      "Add new Task",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 21),
+                      textAlign: TextAlign.center,
+                    ))
+                  ],
+                ),
               ),
-            ),
-            const Text("Task Title"),
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 40, vertical: 20),
-              child: TextField(
-                decoration:
-                    InputDecoration(filled: true, fillColor: Colors.white),
+              const Padding(
+                  padding: EdgeInsets.only(top: 10), child: Text("Task Title")),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 40),
+                child: TextField(
+                  decoration:
+                      InputDecoration(filled: true, fillColor: Colors.white),
+                ),
               ),
-            ),
-            Row(
-              children: [
-                const Text("Category"),
-                GestureDetector(
-                  onTap:(){
-
-                  }, 
-                  child: Image.asset("lib/assets/images/task.png"),
-                )
-              ],
-            )
-          ],
+              Padding(
+                padding: const EdgeInsets.only(top: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    const Text("Category"),
+                    GestureDetector(
+                      onTap: () {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                            duration: Duration(milliseconds: 300),
+                            content: Text("Category Selected"),
+                          ),
+                        );
+                      },
+                      child: Image.asset("lib/assets/images/task.png"),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                            duration: Duration(milliseconds: 300),
+                            content: Text("Category Selected"),
+                          ),
+                        );
+                      },
+                      child: Image.asset("lib/assets/images/goal.png"),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                            duration: Duration(milliseconds: 300),
+                            content: Text("Category Selected"),
+                          ),
+                        );
+                      },
+                      child: Image.asset("lib/assets/images/event.png"),
+                    )
+                  ],
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(top: 10),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Column(
+                        children: [
+                          Text("Date"),
+                          Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 20),
+                            child: TextField(
+                              decoration: InputDecoration(
+                                  filled: true, fillColor: Colors.white),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: Column(
+                        children: [
+                          Text("Time"),
+                          Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 20),
+                            child: TextField(
+                              decoration: InputDecoration(
+                                  filled: true, fillColor: Colors.white),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const Padding(
+                  padding: EdgeInsets.only(top: 10), child: Text("Notes")),
+              const SizedBox(
+                height: 300,
+                child: TextField(
+                  expands: true,
+                  maxLines: null,
+                  decoration: InputDecoration(
+                      filled: true, fillColor: Colors.white, isDense: true),
+                ),
+              ),
+              ElevatedButton(onPressed: () {}, child: const Text("Save")),
+            ],
+          ),
         ),
       ),
     );
